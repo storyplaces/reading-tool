e9850bcd6cbb1fd14600c741b1ae05b1a51a613a
Kevin <kevin@algn.uk>
2017-01-06 15:52:04 +0000

    Added extra fields to page Fixed condition running functions

--- src/resources/models/Page.ts
+++ src/resources/models/Page.ts
@@ -40,12 +40,17 @@ import {VariableCollection} from "../collections/VariableCollection";
 import {LocationInformation} from "../gps/LocationInformation";
 import {LocationCollection} from "../collections/LocationCollection";
 import {ConditionCollection} from "../collections/ConditionCollection";
+import {FunctionCollection} from "../collections/FunctionCollection";
 
 @inject(TypeChecker)
 export class Page extends BaseModel {
-
     private _name: string;
-    private _conditions: Array<BaseCondition>;
+    private _conditions: Array<string>;
+    private _content: string;
+    private _pageTransition: string;
+    private _hintDescription: string;
+    private _hintLocations: Array<string>;
+    private _functions: Array<string>
 
     private _isViewable: boolean;
     private _isReadable: boolean;
@@ -55,11 +60,16 @@ export class Page extends BaseModel {
         this.fromObject(data);
     }
 
-    public fromObject(data: any = {id: undefined, name: undefined, conditions: undefined}) {
+    public fromObject(data: any = {id: undefined, name: undefined, conditions: undefined, content: undefined, pageTransition: undefined, hint: {locations:undefined, description:undefined}, functions: undefined}) {
         this.typeChecker.validateAsObjectAndNotArray("Data", data);
         this.id = data.id;
         this.name = data.name;
         this.conditions = data.conditions;
+        this.content = data.content;
+        this.pageTransition = data.pageTransition;
+        this.hintLocations = data.hint.locations;
+        this.hintDescription = data.hint.description;
+        this.functions = data.functions;
     }
 
     public toJSON() {
@@ -67,14 +77,22 @@ export class Page extends BaseModel {
             id: this.id,
             name: this.name,
             conditions: this.conditions,
+            content: this.content,
+            functions: this.functions,
+            pageTransition: this.pageTransition,
+            hint: {
+                description: this.hintDescription,
+                locations: this.hintLocations
+            }
         }
     }
 
-    get conditions(): Array<BaseCondition> {
+    get conditions(): Array<string> {
         return this._conditions;
     }
 
-    set conditions(value: Array<BaseCondition>) {
+    set conditions(value: Array<string>) {
+        this.typeChecker.isUndefinedOrArrayOf("Conditions", value, "string");
         this._conditions = value;
     }
 
@@ -85,6 +103,57 @@ export class Page extends BaseModel {
     set name(value: string) {
         this.typeChecker.validateAsStringOrUndefined("Name", value);
         this._name = value;
+    }
+
+    get functions(): Array<string> {
+        return this._functions;
+    }
+
+    set functions(value: Array<string>) {
+        this.typeChecker.isUndefinedOrArrayOf("Functions", value, "string");
+        this._functions = value;
+    }
+
+    get hintLocations(): Array<string> {
+        return this._hintLocations;
+    }
+
+    set hintLocations(value: Array<string>) {
+        this.typeChecker.isUndefinedOrArrayOf("Hint Locations", value, "string");
+        this._hintLocations = value;
+    }
+
+    get hintDescription(): string {
+        return this._hintDescription;
+    }
+
+    set hintDescription(value: string) {
+        this.typeChecker.validateAsStringOrUndefined("Hint Text", value);
+        this._hintDescription = value;
+    }
+
+    get pageTransition(): string {
+        return this._pageTransition;
+    }
+
+    set pageTransition(value: string) {
+        this.typeChecker.validateAsStringOrUndefined("Page Transition", value);
+
+        if (value != "next" && value != "end") {
+            throw TypeError("Page transition must only be next or end");
+        }
+
+        this._pageTransition = value;
+    }
+
+
+    get content(): string {
+        return this._content;
+    }
+
+    set content(value: string) {
+        this.typeChecker.validateAsStringOrUndefined("Content", value);
+        this._content = value;
     }
 
     get isReadable(): boolean {
@@ -103,19 +172,44 @@ export class Page extends BaseModel {
         this._isViewable = value;
     }
 
+
     // A page is viewable if all conditions for the page are valid, not including any location conditions.
     public updateViewable(variables: VariableCollection, conditions: ConditionCollection) {
-        this.isViewable = this.conditions.every((condition) => {
-            return condition.execute(variables, conditions);
+        this.isViewable = this.conditions.every((conditionId) => {
+            return this.getCondition(conditions, conditionId).execute(variables, conditions);
         });
-
     }
 
     // A page is readable if all conditions for the page are valid, including all location conditions.
     public updateReadable(variables: VariableCollection, conditions: ConditionCollection, locations: LocationCollection, userLocation: LocationInformation) {
-        this.isReadable = this.conditions.every((condition) => {
-            return condition.execute(variables, conditions, locations, userLocation);
+        this.isReadable = this.conditions.every((conditionId) => {
+            return this.getCondition(conditions, conditionId).execute(variables, conditions, locations, userLocation);
         });
+    }
 
+    public executeFunctions(variables: VariableCollection, conditions: ConditionCollection, locations: LocationCollection, userLocation: LocationInformation, functions: FunctionCollection) {
+        this.functions.forEach((functionId) => {
+            this.getFunction(functions, functionId).execute(variables, conditions, locations, userLocation);
+        });
+    }
+
+    private getCondition(conditions: ConditionCollection, conditionId) {
+        let condition = conditions.get(conditionId);
+
+        if (!condition) {
+            throw Error("Condition " + conditionId + "was not found");
+        }
+
+        return condition;
+    }
+
+    private getFunction(functions: FunctionCollection, functionId) {
+        let func = functions.get(functionId);
+
+        if (!func) {
+            throw Error("Function " + functionId + "was not found");
+        }
+
+        return func;
     }
 }
