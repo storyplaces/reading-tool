1640c84d160371d2494b9193cfb1d987ab9dc3be
Kevin <kevin@algn.uk>
2017-04-20 11:29:21 +0100

    Fixed loading of non existent stories

--- src/pages/story-detail-page.ts
+++ src/pages/story-detail-page.ts
@@ -2,37 +2,51 @@
  * Created by andy on 28/11/16.
  */
 import {StoryConnector} from "../resources/store/StoryConnector";
-import {autoinject, computedFrom} from "aurelia-framework";
+import {autoinject} from "aurelia-framework";
 import {Story} from "../resources/models/Story";
 import {LoggingHelper} from "../resources/logging/LoggingHelper";
+import {Router} from "aurelia-router";
 
 @autoinject()
 export class StoryDetailPage {
 
     storyId: string;
+    story: Story;
 
     constructor(private storyConnector: StoryConnector,
-                private loggingHelper: LoggingHelper) {
+                private loggingHelper: LoggingHelper,
+                private router: Router) {
     }
 
-    @computedFrom('storyConnector.all', 'storyId')
-    get story(): Story {
-        console.log("get story");
-        return this.storyConnector.byId(this.storyId);
-    }
+    // @computedFrom('storyConnector.all', 'storyId')
+    // get story(): Story {
+    //     console.log("get story");
+    //     return this.storyConnector.byId(this.storyId);
+    // }
 
     activate(params) {
         this.storyId = params.storyId;
 
+        this.story = this.storyConnector.byId(this.storyId);
+
         if (this.story === undefined) {
-            this.storyConnector.fetchById(params.storyId);
+            this.storyConnector.fetchById(params.storyId)
+                .then(() => {
+                    this.story = this.storyConnector.byId(this.storyId);
+
+                    if (this.story === undefined) {
+                        this.router.navigateToRoute('home');
+                    }
+                })
+                .catch(() => {
+                    this.router.navigateToRoute('home');
+                });
         }
+
         this.loggingHelper.logViewStory(this.storyId);
     }
 
     detatched() {
         this.storyId = undefined;
     }
-
-
 }
